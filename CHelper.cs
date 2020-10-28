13628a27ccba35ad65a5f1c2c06ea4fe6aa1d11a
John Doe <k1x-root@web.de>
2020-10-28 21:50:26 +0100

    Add files via upload

---
+++ CHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedRain
+{
+    class CHelper
+    {
+        public static void Log(string s, string file)
+        {
+            using (StreamWriter sw = new StreamWriter(file, true))
+            {
+                sw.WriteLine("[---" + DateTime.Now + "---]" + Environment.NewLine + s + Environment.NewLine + "[-------------------------]" + Environment.NewLine);
+                sw.Close();
+            }
+        }
+    }
+}
